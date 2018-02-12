e8db1b9764c74870d5e3e45ef205bcb796f81268
Samuel Woodbridge <s-bridge@live.co.uk>
2018-02-12 11:37:28 +0000

    [Init] - Initial commit

---
+++ ios_ci_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_ci_test
+//
+//  Created by Samuel on 12/02/2018.
+//  Copyright © 2018 Propeller Communications. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
